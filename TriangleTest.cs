1983ab2571f252759daf71e497c9fa74166d8471
Shaik Mathar Syed <shaik.mathar99@gmail.com>
2024-03-06 20:57:27 -0500

    scalene triangle tests added

--- TriangleTest.cs
+++ TriangleTest.cs
@@ -86,4 +86,97 @@ namespace TriangleTest
             Assert.AreEqual(expected, result);
         }
     }
+
+    public class ScaleneTriangleTest
+    {
+        [Test]
+        public void ScaleneTriangleUnitTestCaseOne()
+        {
+            // The test is in the A-A-A format
+            // Arrange
+            int firstSide = 70;
+            int secondSide = 30;
+            int thirdSide = 80;
+
+            string expected = "Scalene triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ScaleneTriangleUnitTestCaseTwo()
+        {
+            // The test is in the A-A-A format
+            // Arrange
+            int firstSide = 50;
+            int secondSide = 60;
+            int thirdSide = 40;
+
+            string expected = "Scalene triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ScaleneTriangleUnitTestCaseThree()
+        {
+            // The test is in the A-A-A format
+            // Arrange
+            int firstSide = 60;
+            int secondSide = 55;
+            int thirdSide = 65;
+
+            string expected = "Scalene triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ScaleneTriangleUnitTestCaseFour()
+        {
+            // The test is in the A-A-A format
+            // Arrange
+            int firstSide = 45;
+            int secondSide = 80;
+            int thirdSide = 55;
+
+            string expected = "Scalene triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void ScaleneTriangleUnitTestCaseFive()
+        {
+            // The test is in the A-A-A format
+            // Arrange
+            int firstSide = 65;
+            int secondSide = 80;
+            int thirdSide = 35;
+
+            string expected = "Scalene triangle";
+
+            // Act
+            string result = Triangle.AnalyzeTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
 }
